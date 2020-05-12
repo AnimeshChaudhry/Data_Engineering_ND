ddb7279d9fc97499e57079053596c13e90a79493
Animesh Chaudhry <animesh.chaudhry1997@gmail.com>
2020-05-11 18:28:02 -0700

    create-table operator fixed

--- Data Pipelines - Airflow/plugins/operators/create_table.py
+++ Data Pipelines - Airflow/plugins/operators/create_table.py
@@ -5,24 +5,21 @@ from airflow.utils.decorators import apply_defaults
 
 class CreateTableOperator(BaseOperator):
     ui_color = '#358140'
-    
+
     @apply_defaults
-    def __init__(self, redshift_conn_id="", *args, **kwargs):
+    def __init__(self, redshift_conn_id = "", *args, **kwargs):
         
-        super(CreateTableOperator, sekf).__init__(*args, **kwargs)
+        super(CreateTableOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         
     def execute(self, context):
-        self.log.info("Creating Postgress SQL hook")
+        self.log.info('Creating Postgres SQL Hook')
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
         self.log.info("Postgress SQL hook created")
-        
-        self.log.info("Creating tables in redshift")
-        
-        # Open and read the file as a single buffer
-        fd = open('/home/workspace/airflow/create_tables.sql', 'r')
-        sqlFile = fd.read()
-        redshift.run(sqlFile)
+
+        self.log.info('Executing creating tables in Redshift.')
+        queries =  open('/home/workspace/airflow/create_tables.sql', 'r').read()
+        redshift.run(queries)
         self.log.info("Done creating tables.")
-        fd.close()
-      
+        
+        self.log.info("Tables created ")
